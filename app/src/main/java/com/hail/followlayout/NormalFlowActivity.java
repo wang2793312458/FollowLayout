cb03d5c39c593382e7c27de0fa599acbace36483
wz2793312458 <wz2793312458@163.com>
2017-08-14 11:35:03 +0800

    长按删除

--- app/src/main/java/com/hail/followlayout/NormalFlowActivity.java
+++ app/src/main/java/com/hail/followlayout/NormalFlowActivity.java
@@ -1,11 +1,14 @@
 package com.hail.followlayout;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.library.AutoFlowLayout;
 
@@ -46,6 +49,35 @@ public class NormalFlowActivity extends AppCompatActivity implements View.OnClic
             tvAttrTag.setText(mData[i]);
             mFlowLayout.addView(item);
         }
+        mFlowLayout.setOnItemClickListener(new AutoFlowLayout.OnItemClickListener() {
+            @Override
+            public void onItemClick(int position, View view) {
+                Toast.makeText(NormalFlowActivity.this, mData[position], Toast.LENGTH_SHORT).show();
+            }
+        });
+        mFlowLayout.setOnLongItemClickListener(new AutoFlowLayout.OnLongItemClickListener() {
+            @Override
+            public void onLongItemClick(final int position, View view) {
+                AlertDialog dialog = new AlertDialog.Builder(NormalFlowActivity.this)
+                        .setTitle("long click")
+                        .setMessage("You will delete this tag!")
+                        .setPositiveButton("Delete", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                mFlowLayout.setLineCenter(false);
+                                mFlowLayout.deleteView(position);
+                            }
+                        })
+                        .setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.dismiss();
+                            }
+                        })
+                        .create();
+                dialog.show();
+            }
+        });
     }
 
     @Override
